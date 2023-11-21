ee627a759bd1cc860fcd661dfe5f38a91a603be6
Andres Rojas <127151298+AndresRA13@users.noreply.github.com>
2023-11-21 15:26:03 -0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,43 @@
 
-let form = document.getElementById('Form');
-let valorOne = document.getElementById('valorOne');
-let valorTwo = document.getElementById('valorTwo');
-let inputSelect = document.getElementById('operacion');
+
+
+// Obtener elementos del DOM
+let form = document.querySelector('.inputs');
+let btnPresupuesto = document.querySelector('.pres_add');
 let result = document.getElementById('Resultado');
 
+// Evento de envío del formulario
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    valorOneValue = parseFloat(valorOne.value);
-    valorTwoValue = parseFloat(valorTwo.value);
-    operacion = inputSelect.value;
-    let resultado;
-    switch (operacion) {
-        case '+':
-            resultado = '$ ' +  valorOneValue + valorTwoValue;
-            break;
-        case '-':
-            resultado = valorOneValue - valorTwoValue;
-            break;
-        default:
-            resultado = 'invalida';
 
+    // Obtener el valor actual del botón de presupuesto
+    let btnPresupuestoActive = parseFloat(btnPresupuesto.value);
+
+    // Obtener el resultado almacenado en el localStorage
+    const storeResult = localStorage.getItem('total');
+
+    // Verificar si el resultado almacenado es válido
+    if (storeResult !== null) {
+        // Sumar el valor del botón de presupuesto al resultado almacenado
+        let nuevoTotal = parseFloat(storeResult) + btnPresupuestoActive;
+
+        // Mostrar el nuevo total en el resultado y actualizar el localStorage
+        result.innerHTML = '$ ' + nuevoTotal.toFixed(2);
+        localStorage.setItem('total', nuevoTotal.toFixed(2));
+    } else {
+        // Si no hay resultado almacenado, mostrar un mensaje de error o realizar alguna acción
+        console.error('No se encontró un resultado almacenado en el localStorage');
     }
-    localStorage.setItem('Total', JSON.stringify(resultado));
-    result.innerHTML = resultado;
-})
+});
+
+// Cargar el resultado almacenado al cargar la página
+const storeResult = localStorage.getItem('total');
+if (storeResult !== null) {
+    result.innerHTML = '$ ' + storeResult;
+}
+
+
+
 
 
 
