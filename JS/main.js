c3ec651962e8230e9e280844832b044db9ccd4fb
Andres Rojas <127151298+AndresRA13@users.noreply.github.com>
2023-11-22 15:06:23 -0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
-
+/*FUNCIOANLIDAD DE LOS MODALES */
 /*funcionalidad del modal de agregar presupuesto */
-const Close =  document.querySelector('.close');
+const Close = document.querySelector('.close');
 const abrir = document.querySelector('.addPresupuesto');
 const modal = document.querySelector('.modal_one');
 
@@ -16,35 +16,37 @@ Close.addEventListener('click', () => {
 
 
 /**Modal de agregar ganancias*/
-const closeBtn =  document.querySelector('.closeBtn');
-const abrirBtn =  document.querySelector('.abrirBtn');
-const modalBit =  document.querySelector('.modal__add');
-abrirBtn.addEventListener('click', (e) =>{
+const closeBtn = document.querySelector('.closeBtn');
+const abrirBtn = document.querySelector('.abrirBtn');
+const modalBit = document.querySelector('.modal__add');
+
+abrirBtn.addEventListener('click', (e) => {
     e.preventDefault();
     modalBit.classList.add('mostrar');
     closeBtn.classList.add('mostrar');
-    
-    
+
+
 });
-closeBtn.addEventListener('click', (e)=> {
+closeBtn.addEventListener('click', (e) => {
     e.preventDefault();
     modalBit.classList.remove('mostrar');
     closeBtn.classList.remove('mostrar');
 
 });
 
+
 /**Modal de restar las ganancias o gastos*/
-const closeBTN =  document.querySelector('.closeBTN');
-const openBTN =  document.querySelector('.openBTN');
-const modalDelete =  document.querySelector('.modal_delete');
-openBTN.addEventListener('click', (e) =>{
+const closeBTN = document.querySelector('.closeBTN');
+const openBTN = document.querySelector('.openBTN');
+const modalDelete = document.querySelector('.modal_delete');
+openBTN.addEventListener('click', (e) => {
     e.preventDefault();
     modalDelete.classList.add('mostrar');
     closeBTN.classList.add('mostrar');
-    
-    
+
+
 });
-closeBTN.addEventListener('click', (e)=> {
+closeBTN.addEventListener('click', (e) => {
     e.preventDefault();
     modalDelete.classList.remove('mostrar');
     closeBTN.classList.remove('mostrar');
@@ -52,51 +54,136 @@ closeBTN.addEventListener('click', (e)=> {
 });
 
 
-function formatToCOP(valor) {
-    return valor.toLocaleString("es-CO", { style: "currency", currency: "COP" });
-}
-// Obtener elementos del DOM
-let form = document.querySelector('.inputs');
-let btnPresupuesto = document.querySelector('.pres_add');
-let result = document.getElementById('Resultado');
 
-// Evento de envío del formulario
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // Obtener el valor actual del botón de presupuesto
-    let btnPresupuestoActive = parseFloat(btnPresupuesto.value);
-
-    // Obtener el resultado almacenado en el localStorage
-    const storeResult = localStorage.getItem('total');
-
-    // Verificar si el resultado almacenado es válido
-    if (storeResult !== null) {
-        // Sumar el valor del botón de presupuesto al resultado almacenado
-        let nuevoTotal = parseFloat(storeResult) + btnPresupuestoActive;
-
-        // Formatear el nuevo total a moneda colombiana y mostrarlo en el resultado
-        result.innerHTML = formatToCOP(nuevoTotal);
-        
-        // Actualizar el localStorage con el nuevo total
-        localStorage.setItem('total', nuevoTotal.toFixed(1));
-    } else {
-        // Si no hay resultado almacenado, mostrar un mensaje de error o realizar alguna acción
-        console.error('No se encontró un resultado almacenado en el localStorage');
-    }
-
-    // Ocultar el modal
-    modal.classList.remove('mostrar');
-    Close.classList.remove('mostrar');
+document.addEventListener('DOMContentLoaded', function () {
+   // Evento de envío del formulario de Presupuesto
+document.querySelector('.inputs').addEventListener('submit', function (e) {
+  e.preventDefault();
+  agregarAlHistorial('Presupuesto', '', parseFloat(document.querySelector('.pres_add').value));
 });
 
-// Cargar el resultado almacenado al cargar la página
-const storeResult = localStorage.getItem('total');
-if (storeResult !== null) {
-    // Formatear el resultado a moneda colombiana y mostrarlo en la página
-    result.innerHTML = formatToCOP(parseFloat(storeResult));
-}
+  // Evento de envío del formulario de Ganancia
+document.querySelector('.newInputs').addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Obtener el valor del campo de descripción
+  let descripcion = document.querySelector('.newInputs .descrip').value;
+  // Llamar a la función agregarAlHistorial con el tipo, mensaje y monto
+  agregarAlHistorial('Ganancia', descripcion, parseFloat(document.querySelector('.newInputs input[type="text"]').value));
+});
 
+// Evento de envío del formulario de Gasto
+document.querySelector('.deleteForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Obtener el valor del campo de descripción
+  let descripcion = document.querySelector('.deleteForm .descripcion').value;
+  // Llamar a la función agregarAlHistorial con el tipo, mensaje y monto
+  agregarAlHistorial('Gasto', descripcion, -parseFloat(document.querySelector('.deleteForm input[type="text"]').value));
+});
+
+   
+  
+    // Evento para eliminar un elemento del historial
+    document.addEventListener('click', function (e) {
+      if (e.target.classList.contains('delete_btn')) {
+        eliminarElementoHistorial(e.target.closest('.history_item'));
+      }
+    });
+  
+    // Función para agregar elementos al historial y localStorage
+    function agregarAlHistorial(tipo, mensaje, monto) {
+      // Crear un objeto que representa un elemento del historial
+      let historialItem = { tipo: tipo, mensaje: mensaje, monto: monto };
+  
+      // Obtener el historial actual del localStorage
+      let historial = JSON.parse(localStorage.getItem('historial')) || [];
+  
+      // Agregar el nuevo item al historial
+      historial.push(historialItem);
+  
+      // Almacenar el historial actualizado en el localStorage
+      localStorage.setItem('historial', JSON.stringify(historial));
+  
+      // Actualizar el valor total
+      actualizarTotal();
+  
+      // Mostrar el historial en la página
+      mostrarHistorialEnPagina();
+    }
+  
+    // Función para eliminar un elemento del historial
+    function eliminarElementoHistorial(elemento) {
+      // Obtener el índice del elemento en el historial
+      let indice = Array.from(elemento.parentNode.children).indexOf(elemento);
+  
+      // Obtener el historial actual del localStorage
+      let historial = JSON.parse(localStorage.getItem('historial')) || [];
+  
+      // Eliminar el elemento del historial
+      historial.splice(indice, 1);
+  
+      // Almacenar el historial actualizado en el localStorage
+      localStorage.setItem('historial', JSON.stringify(historial));
+  
+      // Actualizar el valor total
+      actualizarTotal();
+  
+      // Mostrar el historial en la página
+      mostrarHistorialEnPagina();
+    }
+  
+    // Función para mostrar el historial en la página
+    function mostrarHistorialEnPagina() {
+      // Obtener el historial actual del localStorage
+      let historial = JSON.parse(localStorage.getItem('historial')) || [];
+    
+      // Obtener el div del historial
+      let historialDiv = document.querySelector('.history');
+    
+      // Limpiar el contenido actual del historialDiv
+      historialDiv.innerHTML = '';
+    
+      // Iterar sobre cada elemento del historial y mostrarlo en la página
+      historial.forEach(item => {
+        let nuevoElemento = document.createElement('div');
+        nuevoElemento.className = 'history_item';
+    
+        // Agregar la clase "gasto" si el tipo es Gasto
+        if (item.tipo === 'Gasto') {
+          nuevoElemento.classList.add('gasto');
+        }
+    
+        nuevoElemento.innerHTML = `<span>${item.tipo}: <br> ${item.mensaje}</span><span class="amount">${formatToCOP(item.monto)}</span><button class="delete_btn">Eliminar</button>`;
+        historialDiv.appendChild(nuevoElemento);
+      });
+    }
+    
+  
+    // Función para actualizar el valor total
+    function actualizarTotal() {
+      // Obtener el historial actual del localStorage
+      let historial = JSON.parse(localStorage.getItem('historial')) || [];
+  
+      // Calcular el nuevo total sumando los montos del historial
+      let nuevoTotal = historial.reduce((total, item) => total + item.monto, 0);
+  
+      // Mostrar el nuevo total en la página
+      document.getElementById('Resultado').innerHTML = formatToCOP(nuevoTotal);
+  
+      // Actualizar el total en el localStorage
+      localStorage.setItem('total', nuevoTotal);
+    }
+  
+    // Llamar a la función al cargar la página para mostrar el historial existente
+    mostrarHistorialEnPagina();
+    // Llamar a la función al cargar la página para actualizar el valor total
+    actualizarTotal();
+  });
+  
+  // Función para formatear a moneda colombiana
+  function formatToCOP(valor) {
+    return valor.toLocaleString("es-CO", { style: "currency", currency: "COP" });
+  }
+  
 
 
 
