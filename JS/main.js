d848a82cc652211c7046c0cafd857acedd14fa8b
Andres Rojas <127151298+AndresRA13@users.noreply.github.com>
2023-11-22 10:11:54 -0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -14,6 +14,29 @@ Close.addEventListener('click', () => {
     Close.classList.remove('mostrar');  // Quita la clase 'mostrar' de Close
 });
 
+
+/**Modal de agregar ganancias*/
+const closeBtn =  document.querySelector('.closeBtn');
+const abrirBtn =  document.querySelector('.abrirBtn');
+const modalBit =  document.querySelector('.modal__add');
+abrirBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalBit.classList.add('mostrar');
+    closeBtn.classList.add('mostrar');
+    
+    
+});
+closeBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    modalBit.classList.remove('mostrar');
+    closeBtn.classList.remove('mostrar');
+
+})
+
+
+function formatToCOP(valor) {
+    return valor.toLocaleString("es-CO", { style: "currency", currency: "COP" });
+}
 // Obtener elementos del DOM
 let form = document.querySelector('.inputs');
 let btnPresupuesto = document.querySelector('.pres_add');
@@ -34,13 +57,17 @@ form.addEventListener('submit', (e) => {
         // Sumar el valor del botón de presupuesto al resultado almacenado
         let nuevoTotal = parseFloat(storeResult) + btnPresupuestoActive;
 
-        // Mostrar el nuevo total en el resultado y actualizar el localStorage
-        result.innerHTML = '$ ' + nuevoTotal.toFixed(2);
-        localStorage.setItem('total', nuevoTotal.toFixed(2));
+        // Formatear el nuevo total a moneda colombiana y mostrarlo en el resultado
+        result.innerHTML = formatToCOP(nuevoTotal);
+        
+        // Actualizar el localStorage con el nuevo total
+        localStorage.setItem('total', nuevoTotal.toFixed(1));
     } else {
         // Si no hay resultado almacenado, mostrar un mensaje de error o realizar alguna acción
         console.error('No se encontró un resultado almacenado en el localStorage');
     }
+
+    // Ocultar el modal
     modal.classList.remove('mostrar');
     Close.classList.remove('mostrar');
 });
@@ -48,9 +75,9 @@ form.addEventListener('submit', (e) => {
 // Cargar el resultado almacenado al cargar la página
 const storeResult = localStorage.getItem('total');
 if (storeResult !== null) {
-    result.innerHTML = '$ ' + storeResult;
+    // Formatear el resultado a moneda colombiana y mostrarlo en la página
+    result.innerHTML = formatToCOP(parseFloat(storeResult));
 }
-
 
 
 
